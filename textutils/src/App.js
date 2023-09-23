7708578c2edafefc34afc28155c2080044ae5222
Tuhin114 <tuhinpoddar114@gmail.com>
2023-09-23 17:38:15 +0530

    Initial commit

---
+++ textutils/src/App.js
@@ -0,0 +1,20 @@
+
+// import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from './components/TextForm'
+
+function App() {
+  return (
+    <>
+      {/* <Navbar title="TextUtils2"  aboutText="About Text"/> */}
+      <Navbar/>
+      <div className='container my-3'>
+        <TextForm heading="Enter the text to analyze"/>
+        {/* <About/> */}
+      </div>
+      
+    </>
+  );
+}
+
+export default App;

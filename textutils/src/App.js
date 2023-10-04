63972223e9af9be712cd7e0636ff29f58347b823
Tuhin114 <tuhinpoddar114@gmail.com>
2023-10-04 18:40:29 +0530

    Updated

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,13 +1,16 @@
 
 // import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextForm from './components/TextForm'
+import TextForm from './components/TextForm';
+import React, { useState} from "react";
 
 function App() {
+  const [mode, setMode] = useState('dark') ;//whether dark mode is enabled or not
   return (
     <>
       {/* <Navbar title="TextUtils2"  aboutText="About Text"/> */}
-      <Navbar/>
+      {/* <Navbar/> */}
+      <Navbar title="TextUtils" mode={mode} />
       <div className='container my-3'>
         <TextForm heading="Enter the text to analyze"/>
         {/* <About/> */}
--- textutils/src/components/Navbar.js
+++ textutils/src/components/Navbar.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark-subtle ">
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             {props.title}
@@ -33,7 +33,7 @@ export default function Navbar(props) {
                 </a>
               </li>
             </ul>
-            <form className="d-flex" role="search">
+            {/* <form className="d-flex" role="search">
               <input
                 className="form-control me-2"
                 type="search"
@@ -43,7 +43,11 @@ export default function Navbar(props) {
               <button className="btn btn-outline-primary" type="submit">
                 Search
               </button>
-            </form>
+            </form> */}
+            <div class="form-check form-switch text-light">
+              <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+              <label class="form-check-label" for="flexSwitchCheckDefault">Enable DarkMode</label>
+            </div>
           </div>
         </div>
       </nav>

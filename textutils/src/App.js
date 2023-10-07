0af96b7b571f68c3486528af27f4f8c5dc41f7bf
Tuhin114 <tuhinpoddar114@gmail.com>
2023-10-07 18:56:12 +0530

    Added react router....syntax is ok...but some internal issue..for which is not working...please copy all the syntax and in terminal npm install react-router-dom

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,60 +1,66 @@
-
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-// import About from "./components/About"; // Uncomment this line if you intend to use the About component
+import About from "./components/About";
 import Alert from "./components/alert";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-  const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
+  const [mode, setMode] = useState("light"); // whether dark mode is enabled or not
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message,type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
-    })
+      type: type,
+    });
     setTimeout(() => {
       setAlert(null);
     }, 1500);
-  }
+  };
 
   const toggleMode = () => {
-    if (mode === 'light') {
-      setMode('dark');
-      document.body.style.backgroundColor = 'grey';
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
       showAlert("Dark mode activated", "success");
-      document.title = 'Textutils - Dark Mode';
-      
-      //For changing titles repeatedly
-      /*setInterval(() => {
-        document.title = 'Textutils is Amazing';
-      }, 2000);
-      
-      setInterval(() => {
-        document.title = 'Install Textutils now!!';
-      }, 1500);*/
-      
+      document.title = "Textutils - Dark Mode";
     } else {
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
+      setMode("light");
+      document.body.style.backgroundColor = "white";
       showAlert("Light mode activated", "success");
-      document.title = 'Textutils - Light Mode';
+      document.title = "Textutils - Light Mode";
     }
-  }
+  };
 
   return (
-    <>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert Alert={alert}/>
-      <div className='container my-3'>
-        <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />
-        {/* Uncomment the following line if you intend to use the About component */}
-        {/* <About /> */}
+    <BrowserRouter>
+      <Navbar
+        title="TextUtils"
+        aboutText="About Us"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+
+      <Alert alert={alert} />
+      <div className={`container my-3 ${mode}`}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <TextForm
+                showAlert={showAlert}
+                heading="Enter the text to analyze below"
+                mode={mode}
+              />
+            }
+          />
+          <Route path="/About" element={<About />} />
+        </Routes>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
